adecc7e5655006c7a4fc69fddc25f064a9f9ddca
Brian Watson <brian_watson@esri.com>
2016-10-10 16:10:21 -0400

    Modified to ignore certificate warnings and support federated Server/Portal configuration

--- BackupSite_sample.py
+++ BackupSite_sample.py
@@ -1,13 +1,14 @@
 import adminfunctions
 
-#Parameters
-AGSRestEndpoint = "http://localhost:6080/arcgis/rest"
-username = "admin"
-password = "admin"
+#Parameters - use portal credentials for federated setup
+AGSRestEndpoint = "http://myserver.mysite.com/arcgis/rest"
+username = "siteadmin"
+password = "siteadmin"
+referer = "http://referer.mysite.com/arcgis/rest"
 
 ###Script execution - Where functions are executed.
 endpoints = adminfunctions.defineEndpoints(AGSRestEndpoint)
-token = adminfunctions.getToken(endpoints["TokenUrl"],username,password)["token"]
+token = adminfunctions.getToken(endpoints["TokenUrl"],username,password, referer)["token"]
 
 backup = adminfunctions.backupSite(endpoints["Admin"],token,r"C:\temp")
 
@@ -15,4 +16,4 @@ if backup['outcome'] != "success":
     print backup['messages']
 else:
     print "Backup Created: " + backup['messages']
-    
+
--- UpdateServiceTimeouts_sample.py
+++ UpdateServiceTimeouts_sample.py
@@ -1,17 +1,19 @@
 import adminfunctions
 
-#Parameters
+#Parameters - use portal credentials for federated setup
 AGSRestEndpoint = "http://myserver.mysite.com/arcgis/rest"
-username = "admin"
-password = "admin"
+username = "siteadmin"
+password = "siteadmin"
+referer = "http://referer.mysite.com/arcgis/rest"
 
 ###Script execution - Where functions are executed.
 endpoints = adminfunctions.defineEndpoints(AGSRestEndpoint)
-token = adminfunctions.getToken(endpoints["TokenUrl"],username,password)["token"]
+token = adminfunctions.getToken(endpoints["TokenUrl"],username,password, referer)["token"]
 
 services = adminfunctions.getServiceAdminEndpoints(endpoints["Admin"], token)
 for service in services:
     print "Editing " + service + "..."
-    result = adminfunctions.updateTimeouts(service,token,60,300,1800,600)
+    ##serviceAdminUrl, token, maxWaitTime, maxStartupTime, maxIdleTime, maxUsageTime)
+    result = adminfunctions.updateTimeouts(service,token,60,300,300,300)
     print result
     print ""
